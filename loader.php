ae8a7110f0ca47395559bbfa7767ba115fa9ef36
sam-at-github <sgpinkus@gmail.com>
2016-10-30 17:35:51 +1100

    composer.json

--- loader.php
+++ loader.php
@@ -1,1 +1,35 @@
-../loader.php
+<?php
+/**
+ * Convenience loader. Use very optional.
+ */
+
+/**
+ * Simple class loader.
+ */
+class MyLoader
+{
+  protected $bases = [];
+
+  public function __construct(array $bases) {
+    $this->bases = $bases;
+  }
+
+  /**
+   * @input $name FQ classname, without a leading "\".
+   */
+  function load($name) {
+    foreach($this->bases as $prefix => $basePath) {
+      if(strpos($name, $prefix) === 0) {
+        $name = substr($name, strlen($prefix));
+        $path = $basePath . "/" . str_replace("\\", "/", $name) . ".php";
+        include_once $path;
+      }
+    }
+  }
+}
+
+$myPath = dirname(realpath(__FILE__));
+$myLoader = new MyLoader([
+  "JsonDocs" => "$myPath/"]
+);
+spl_autoload_register([$myLoader, "load"]);
